50d62041eda7acf1d1b361fd36fcd8df771f5c69
Anairam Falcon <georgina_gf@live.com.mx>
2023-11-26 17:07:17 -0600

    Student List initialized

---
+++ src/componentes/StudentList.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import StudentModal from './modales/StudentModal'; // Asegúrate de importar el componente de modal
+
+function StudentList() {
+
+}
+
+export default StudentList;
