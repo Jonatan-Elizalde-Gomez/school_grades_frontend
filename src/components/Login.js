72f34fbc26c7078bc26e46f19239c4ef873afe53
IaelChavez <jcs72484@udelasalle.edu.mx>
2023-11-26 15:15:01 -0600

    Login: Lógica e Interfaz

---
+++ src/components/Login.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Aquí supondremos que tu endpoint de autenticación es /login
+      const response = await axios.post('http://127.0.0.1:3001/login', { email, password });
+      // Si el login es exitoso, redirige a la pantalla de StudentList
+      console.log(response)
+      if (response.data.message == "Login successful") {
+        window.location.href = '/students';
+      } else {
+        alert('Invalid credentials');
+      }
+    } catch (error) {
+      alert('Login failed', error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto h-screen flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
