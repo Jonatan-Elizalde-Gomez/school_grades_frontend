c57fdc8e9f4c7f4b98cc83f06352119241a7f9a3
Jesus072466 <jco72466@udelasalle.edu.mx>
2023-11-26 19:59:13 -0600

    Add SubjectModal to SubjectList

--- src/components/SubjectList.js
+++ src/components/SubjectList.js
@@ -1,10 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import SubjectModal from "./modales/SubjectModal";
 
 function SubjectList() {
+  const [subjects, setSubjects] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [currentSubject, setCurrentSubject] = useState(null);
+
+  useEffect(() => {
+    fetchSubjects();
+  }, []);
+
+  const fetchSubjects = async () => {
+    const result = await axios.get("http://127.0.0.1:3001/subjects");
+    setSubjects(result.data);
+  };
+
+  const addSubject = async (subject) => {
+    await axios.post("http://127.0.0.1:3001/subjects", subject);
+    fetchSubjects();
+  };
+
+  const updateSubject = async (subject) => {
+    await axios.put(
+      `http://127.0.0.1:3001/subject-enroll/${currentSubject._id}`,
+      subject
+    );
+    fetchSubjects();
+    setCurrentSubject(null); // Restablece la materia actual después de la actualización
+  };
+
+  const closeModal = () => {
+    setCurrentSubject(null);
+    setShowModal(false);
+  };
 
   return (
-    <div>
+    <div className="container mx-auto mt-8">
+      <SubjectModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        onSubmit={currentSubject ? updateSubject : addSubject}
+        subject={currentSubject}
+        allStudents={allStudents}
+      />
     </div>
   );
 }
